eca0e4d22819e244872dd9ffcd88eb8ad336ccab
Robert Cristiam Faustino de Souza <robert.souza@mundiale.com.br>
2020-11-05 17:14:12 -0300

    fix script remove attendts

--- remove_atten.py
+++ remove_atten.py
@@ -1,8 +1,8 @@
 import uuid
-import urllib.request
+import urllib.request, json
 import json
 from tkinter.filedialog import askopenfilename
-
+import gc
 # buscar atendentes de todos os bots
 # buscar na planilha proximo atendente 
 # verificar bot a bot se ele esta como atendente
@@ -10,13 +10,12 @@ from tkinter.filedialog import askopenfilename
 # ir para proximo
 
 class Attendants:
-    def __init__(self, identity, fullname,email,teams,status,agentSlots):
+    def __init__(self, identity, fullname,email,teams,status):
         self.identity = identity
         self.fullname = fullname
         self.email = email
         self.teams = teams
         self.status = status
-        self.agentSlots = agentSlots
 
 class BotAgents:
     def __init__ (self,auth,attendants):
@@ -53,6 +52,7 @@ def generateIdentify(cpf):
     return f"10{cpf}%40mundiale.com.br@blip.ai"
 
 def getAgentsBot(auth):
+    gc.collect()
     list_agents = []
     body = {
         "id": str(uuid.uuid4()),
@@ -64,13 +64,22 @@ def getAgentsBot(auth):
                              data=bytes(json.dumps(body), encoding="utf-8"), method='POST')
     req.add_header("Authorization", auth)
     req.add_header("Content-Type", "application/json")
-    with urllib.request.urlopen(req) as response:
-        source_settings = response.read().decode('utf-8')
-        response = json.loads(source_settings)
-        for element in response["resource"]["items"]:
-            attendant = Attendants(element["identity"], element["fullname"],element["email"],element["teams"],
-                element["status"],element["agentSlots"])
-            list_agents.append(attendant)
+    try:
+        with urllib.request.urlopen(req) as response:
+            source_settings = response.read().decode('utf-8')
+            response_dic = json.loads(source_settings)
+            for element in response_dic["resource"]["items"]:
+                attendant = Attendants(element["identity"], element["fullname"],element["email"],element["teams"],
+                    element["status"])
+                list_agents.append(attendant)
+    except:
+        with urllib.request.urlopen(req) as response:
+            source_settings = response.read().decode('utf-8')
+            response_dic = json.loads(source_settings)
+            for element in response_dic["resource"]["items"]:
+                attendant = Attendants(element["identity"], element["fullname"],element["email"],element["teams"],
+                    element["status"])
+                list_agents.append(attendant)
     return list_agents
 
 
@@ -80,27 +89,41 @@ arq = open(filename,"r",encoding="utf-8")
 print("Buscando lista de atendentes...\n")
 linhas = arq.readlines()
 a=1
+list_bot_error = []
 for linha in linhas:
     if linha != ";;\n":
         try:
             if(a!=1):
                 auth = linha.split(';')[1]
+                auth = auth.replace("\n","")
                 name = linha.split(';')[0]
                 agents = getAgentsBot(auth)
                 list_bots.append(BotAgents(auth,agents))
         except:
-            print("error")
+            list_bot_error.append(linha)
         a=2
-
 
 listmundiale = askopenfilename()
 arq2 = open(listmundiale,"r",encoding="utf-8")
 print("Buscando lista de atendentes...\n")
 linhas = arq2.readlines()
+list_removed = []
+removeds = {}
 for linha in linhas:
+    qtd=0
     cpf = linha.split(';')[5]
     identify = generateIdentify(cpf)
+    list_removed = []
     for bot in list_bots:
         hasAttendantInBot = checkInBot(identify,bot.attendants)
         if(hasAttendantInBot):
             print(removeAttendant(identify,bot.auth))
+            qtd += 1
+            list_removed.append(bot)
+    removeds[cpf] = list_removed
+    print(f"CPF:{cpf} Removidos:{qtd}")
+    if qtd > 0:
+        print(f"Removido de :{list_removed}")
+final = open("deletados.txt",'w+')
+for key in removeds:
+    final.write(f"{key}:{removeds[key]}\n")
